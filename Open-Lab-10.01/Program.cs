1c9b98ca206f73b8d0c61c303f72f993a84afb9e
sebax774 <bugansebastian@gmail.com>
2020-11-12 21:07:07 +0100

    The Return of the King

--- Open-Lab-10.01/Program.cs
+++ Open-Lab-10.01/Program.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_10._01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book LOTR = new Book("The Return of the King",416);
+            Console.ReadKey();
+        }
+    }
+    class Book
+    {
+        public string title;
+        int pages;
+        public Book(string _title, int _pages)
+        {
+            title = _title;
+            pages = _pages;
+            Console.WriteLine(title + " has " + pages + " pages");
         }
     }
 }
